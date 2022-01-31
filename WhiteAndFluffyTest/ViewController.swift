ac283137552b837733d8f09b97ce4e2ed1b50cd8
Michelangelo <cawabungadude@ya.ru>
2022-01-31 21:37:53 +0300

    Initial Commit

---
+++ WhiteAndFluffyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhiteAndFluffyTest
+//
+//  Created by Михаил Кулагин on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
